a3819d76fef110b3ac2ea9ef1afda0bc2dc9ab0d
Mokkori-Tom <184056355+Mokkori-Tom@users.noreply.github.com>
2025-07-19 08:04:32 +0900

    Update dele.sh

--- dele.sh
+++ dele.sh
@@ -1,4 +1,4 @@
-# run on root(/)
+# run on new root(/)
 git clone https://github.com/Mokkori-Tom/minish
 rm -rf autorebase.bat clang64.ico clangarm64.exe mingw32 mingw32.ini mingw64.ico msys2.exe msys2_shell.cmd ucrt64 ucrt64.ini clang64 clang64.ini clangarm64.ico mingw32.exe mingw64 mingw64.ini msys2.ico ucrt64.exe clang64.exe clangarm64 clangarm64.ini mingw32.ico mingw64.exe msys2.ini ucrt64.ico
 mv minish/* ./
