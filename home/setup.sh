e9cb92cb76e1157397889d1e43a1b0245d1cd26a
Mokkori-Tom <184056355+Mokkori-Tom@users.noreply.github.com>
2025-07-19 07:10:37 +0900

    Update setup.sh

--- home/setup.sh
+++ home/setup.sh
@@ -1,5 +1,5 @@
 cp /etc/pacman.conf /tmp/pacman.conf
-echo 'XferCommand = /usr/bin/curl -k -L -C - -f -o %o %u' >> /tmp/pacman.conf 
+# アクティブにする 'XferCommand = /usr/bin/curl　-L -C - -f -o %o %u' >> /tmp/pacman.conf 
 # (注意:-k SSL認証OFF)
 pacman-key --init --config /tmp/pacman.conf
 pacman-key --populate msys2 --config /tmp/pacman.conf
