d241c8633f6bae0dabb5acc244c4416dec9a3dfa
Mokkori-Tom <184056355+Mokkori-Tom@users.noreply.github.com>
2025-07-19 05:46:05 +0900

    Create minish.go

---
+++ minish.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+    "os"
+    "os/exec"
+)
+
+func main() {
+    cmd := exec.Command("./usr/bin/bash", "bash", "./init.sh")
+    cmd.Stdout = os.Stdout
+    cmd.Stderr = os.Stderr
+    cmd.Stdin = os.Stdin
+    _ = cmd.Run()
+}
