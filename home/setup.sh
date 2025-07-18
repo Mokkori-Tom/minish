87da83af34f6119b289dc7ed91c935ded2531281
Mokkori-Tom <184056355+Mokkori-Tom@users.noreply.github.com>
2025-07-19 08:40:38 +0900

    Update setup.sh

--- home/setup.sh
+++ home/setup.sh
@@ -19,3 +19,5 @@ source .bashrc
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --key-bindings --completion --no-update-rc
+
+dlzip https://go.dev/dl/go1.24.5.windows-amd64.zip $OPT/go
