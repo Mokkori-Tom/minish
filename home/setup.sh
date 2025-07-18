9386fd1fa40c01835fd15709594f3c6ac2a45bc2
Mokkori-Tom <184056355+Mokkori-Tom@users.noreply.github.com>
2025-07-19 06:59:47 +0900

    Update setup.sh

--- home/setup.sh
+++ home/setup.sh
@@ -1,11 +1,11 @@
 cp /etc/pacman.conf /tmp/pacman.conf
 echo 'XferCommand = /usr/bin/curl -k -L -C - -f -o %o %u' >> /tmp/pacman.conf 
 # (注意:-k SSL認証OFF)
-pacman-key --init --/tmp/pacman.conf
-pacman-key --populate msys2 --/tmp/pacman.conf
-pacman -Sy --/tmp/pacman.conf
-pacman -S git --/tmp/pacman.conf
-pacman -S tmux --/tmp/pacman.conf
+pacman-key --init --config /tmp/pacman.conf
+pacman-key --populate msys2 --config /tmp/pacman.conf
+pacman -Sy --config /tmp/pacman.conf
+pacman -S git --config /tmp/pacman.conf
+pacman -S tmux --config /tmp/pacman.conf
 # https://github.com/tmux/tmux/wiki/Getting-Started
 # tmux new
-pacman -S tree --/tmp/pacman.conf
+pacman -S tree --config /tmp/pacman.conf
