e46ff53f97f24f7790982c8bf4330e3d5a9f9a67
Mokkori-Tom <184056355+Mokkori-Tom@users.noreply.github.com>
2025-07-19 08:34:03 +0900

    Update setup.sh

--- home/setup.sh
+++ home/setup.sh
@@ -13,3 +13,6 @@ pacman -S tmux --config /tmp/pacman.conf -y
 # https://github.com/tmux/tmux/wiki/Getting-Started
 # tmux new
 pacman -S tree --config /tmp/pacman.conf -y
+
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install --key-bindings --completion --no-update-rc
