52d7888a44d81813aaff34990beceabd9c4d0a1b
Mokkori-Tom <184056355+Mokkori-Tom@users.noreply.github.com>
2025-07-19 10:31:07 +0900

    Update setup.sh

--- home/setup.sh
+++ home/setup.sh
@@ -14,26 +14,26 @@ pacman -S tmux --config /tmp/pacman.conf --noconfirm
 # tmux new
 pacman -S tree --config /tmp/pacman.conf --noconfirm
 
-source .bashrc
+source $HOME/.bashrc
 
 mkdir $OPT/7zip
 wget -O $OPT/7zip/7zr.exe https://www.7-zip.org/a/7zr.exe
 source .bashrc
 wget -O $OPT/7zip/7z2500-extra.7z https://www.7-zip.org/a/7z2500-extra.7z
 $OPT/7zip/7zr.exe x $OPT/7zip/7z2500-extra.7z -o"$OPT/7zip/"
-source .bashrc
+source ~/.bashrc
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install --key-bindings --completion --no-update-rc
+$HOME/.fzf/install --key-bindings --completion --no-update-rc
 
 dlzip https://go.dev/dl/go1.24.5.windows-amd64.zip $OPT/go
 
 dlzip https://www.python.org/ftp/python/3.12.10/python-3.12.10-embed-amd64.zip $OPT/python
 sed -i 's/^#import site$/import site/' "$OPT/python/python312._pth"
 wget -O $OPT/python/get-pip.py https://bootstrap.pypa.io/pip/get-pip.py
-source .bashrc
+source $HOME/.bashrc
 python $OPT/python/get-pip.py
-source .bashrc
+source $HOME/.bashrc
 python -m pip install --upgrade pip setuptools wheel
 
 dlzip https://github.com/BurntSushi/ripgrep/releases/download/14.1.1/ripgrep-14.1.1-x86_64-pc-windows-gnu.zip $OPT/ripgrep
@@ -46,4 +46,4 @@ dlzip https://github.com/sharkdp/fd/releases/download/v10.2.0/fd-v10.2.0-x86_64-
 dlzip https://github.com/avih/uclip/releases/download/v0.6/uclip.version-0.6.zip $OPT/uclip
 dlzip https://github.com/jesseduffield/lazygit/releases/download/v0.53.0/lazygit_0.53.0_Windows_x86_64.zip $OPT/lazygit
 dlzip https://github.com/charmbracelet/glow/releases/download/v2.1.1/glow_2.1.1_Windows_x86_64.zip $OPT/glow
-source .bashrc
+source $HOME/.bashrc
