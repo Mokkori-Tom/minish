a2cceba576a965cffb49d32aa6d6b02d4e57d7db
Mokkori-Tom <184056355+Mokkori-Tom@users.noreply.github.com>
2025-07-19 21:03:53 +0900

    Update minish.go

--- minish.go
+++ minish.go
@@ -1,14 +1,36 @@
 package main
 
 import (
-    "os"
-    "os/exec"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
 )
 
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
 func main() {
-    cmd := exec.Command("./usr/bin/bash", "./init.sh")
-    cmd.Stdout = os.Stdout
-    cmd.Stderr = os.Stderr
-    cmd.Stdin = os.Stdin
-    _ = cmd.Run()
+	exePath, err := os.Executable()
+	must(err)
+	root := filepath.Dir(exePath)
+
+	bash := filepath.Join(root, "usr", "bin", "bash.exe")
+	if runtime.GOOS != "windows" {
+		bash = filepath.Join(root, "usr", "bin", "bash")
+	}
+	initScript := filepath.Join(root, "init.sh")
+
+	// chdirは無し（カレント維持）
+
+	cmd := exec.Command(bash, initScript)
+	cmd.Args = append(cmd.Args, os.Args[1:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	must(cmd.Run())
 }
