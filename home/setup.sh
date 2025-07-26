9e5514c9089da447f82d24e77e97efac765970ab
Mokkori-Tom <184056355+Mokkori-Tom@users.noreply.github.com>
2025-07-26 17:17:15 +0900

    Update setup.sh

--- home/setup.sh
+++ home/setup.sh
@@ -17,28 +17,31 @@ pacman -S mingw-w64-ucrt-x86_64-neovim --config /tmp/pacman.conf --noconfirm
 pacman -S tree --config /tmp/pacman.conf --noconfirm
 
 mkdir $OPT/7zip
-wget -O $OPT/7zip/7zr.exe https://www.7-zip.org/a/7zr.exe
-wget -O $OPT/7zip/7z2500-extra.7z https://www.7-zip.org/a/7z2500-extra.7z
+export PATH=$PATH:$OPT/7zip
+curl -k --ssl-no-revoke -L -o $OPT/7zip/7zr.exe https://www.7-zip.org/a/7zr.exe
+curl -k --ssl-no-revoke -L -o $OPT/7zip/7z2500-extra.7z https://www.7-zip.org/a/7z2500-extra.7z
 $OPT/7zip/7zr.exe x $OPT/7zip/7z2500-extra.7z -o"$OPT/7zip/"
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 $HOME/.fzf/install --key-bindings --completion --no-update-rc
 
 dlzip https://go.dev/dl/go1.24.5.windows-amd64.zip $OPT/go
+mv $OPT/go/go/* $OPT/go/
 
 dlzip https://www.python.org/ftp/python/3.12.10/python-3.12.10-embed-amd64.zip $OPT/python
 sed -i 's/^#import site$/import site/' "$OPT/python/python312._pth"
-wget -O $OPT/python/get-pip.py https://bootstrap.pypa.io/pip/get-pip.py
+curl -k --ssl-no-revoke -L -o $OPT/python/get-pip.py https://bootstrap.pypa.io/pip/get-pip.py
 $OPT/python/python $OPT/python/get-pip.py
 $OPT/python/Scripts/pip install --upgrade setuptools wheel
 
-dlzip https://github.com/BurntSushi/ripgrep/releases/download/14.1.1/ripgrep-14.1.1-x86_64-pc-windows-gnu.zip $OPT/ripgrep
 dlzip https://github.com/gitui-org/gitui/releases/download/v0.27.0/gitui-win.tar.gz $OPT/gitui
 $OPT/7zip/7za x $OPT/gitui/gitui-win.tar -o$OPT/gitui
+dlzip https://github.com/BurntSushi/ripgrep/releases/download/14.1.1/ripgrep-14.1.1-x86_64-pc-windows-gnu.zip $OPT/ripgrep
 dlzip https://nodejs.org/dist/v22.17.1/node-v22.17.1-win-x64.zip $OPT/node
 dlzip https://github.com/LuaLS/lua-language-server/releases/download/3.15.0/lua-language-server-3.15.0-win32-x64.zip $OPT/lualsp
 dlzip https://github.com/sharkdp/bat/releases/download/v0.25.0/bat-v0.25.0-x86_64-pc-windows-gnu.zip $OPT/bat
 dlzip https://github.com/dandavison/delta/releases/download/0.18.2/delta-0.18.2-x86_64-pc-windows-msvc.zip $OPT/delta
 dlzip https://github.com/sharkdp/fd/releases/download/v10.2.0/fd-v10.2.0-x86_64-pc-windows-gnu.zip $OPT/fd
 dlzip https://github.com/avih/uclip/releases/download/v0.6/uclip.version-0.6.zip $OPT/uclip
+dlzip https://github.com/gitui-org/gitui/releases/download/v0.27.0/gitui-win.tar.gz $OPT/gitui
 dlzip https://github.com/charmbracelet/glow/releases/download/v2.1.1/glow_2.1.1_Windows_x86_64.zip $OPT/glow
